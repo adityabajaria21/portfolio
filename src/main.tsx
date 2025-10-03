62154bdd212834e39a03f458c27c5cd37533520d
Aditya Bajaria <adityabajaria21@gmail.com>
2025-10-03 20:38:29 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,3 @@
-
-import { inject } from '@vercel/analytics';
-
-if (import.meta.env.PROD) {
- inject();
-}
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
